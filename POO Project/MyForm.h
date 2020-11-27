ef0d69776a9b13108560fe86cc701d78f309e072
JF <jeanfrancois.marie@viacesi.fr>
2020-11-27 16:28:27 +0100

    Design Menu Slide

--- POO Project/MyForm.h
+++ POO Project/MyForm.h
@@ -35,6 +35,17 @@ namespace POOProject {
 			}
 		}
 	private: System::Windows::Forms::Panel^ panel1;
+
+	private: System::Windows::Forms::Panel^ panelLogo;
+	private: System::Windows::Forms::Button^ button1;
+	private: System::Windows::Forms::Panel^ panel2;
+	private: System::Windows::Forms::Button^ button6;
+	private: System::Windows::Forms::Button^ button5;
+	private: System::Windows::Forms::Button^ button4;
+	private: System::Windows::Forms::Button^ button3;
+	private: System::Windows::Forms::Button^ button2;
+	private: System::Windows::Forms::PictureBox^ pictureBox1;
+	private: System::Windows::Forms::Button^ button7;
 	protected:
 
 	private:
@@ -50,28 +61,227 @@ namespace POOProject {
 		/// </summary>
 		void InitializeComponent(void)
 		{
+			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(MyForm::typeid));
 			this->panel1 = (gcnew System::Windows::Forms::Panel());
+			this->button7 = (gcnew System::Windows::Forms::Button());
+			this->button6 = (gcnew System::Windows::Forms::Button());
+			this->button5 = (gcnew System::Windows::Forms::Button());
+			this->button4 = (gcnew System::Windows::Forms::Button());
+			this->button3 = (gcnew System::Windows::Forms::Button());
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->panel2 = (gcnew System::Windows::Forms::Panel());
+			this->panelLogo = (gcnew System::Windows::Forms::Panel());
+			this->pictureBox1 = (gcnew System::Windows::Forms::PictureBox());
+			this->panel1->SuspendLayout();
+			this->panelLogo->SuspendLayout();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->BeginInit();
 			this->SuspendLayout();
 			// 
 			// panel1
 			// 
+			this->panel1->AutoScroll = true;
+			this->panel1->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(48)), static_cast<System::Int32>(static_cast<System::Byte>(49)),
+				static_cast<System::Int32>(static_cast<System::Byte>(54)));
+			this->panel1->Controls->Add(this->button7);
+			this->panel1->Controls->Add(this->button6);
+			this->panel1->Controls->Add(this->button5);
+			this->panel1->Controls->Add(this->button4);
+			this->panel1->Controls->Add(this->button3);
+			this->panel1->Controls->Add(this->button2);
+			this->panel1->Controls->Add(this->button1);
+			this->panel1->Controls->Add(this->panel2);
+			this->panel1->Controls->Add(this->panelLogo);
 			this->panel1->Dock = System::Windows::Forms::DockStyle::Left;
 			this->panel1->Location = System::Drawing::Point(0, 0);
 			this->panel1->Name = L"panel1";
-			this->panel1->Size = System::Drawing::Size(167, 614);
+			this->panel1->Size = System::Drawing::Size(200, 561);
 			this->panel1->TabIndex = 0;
+			// 
+			// button7
+			// 
+			this->button7->FlatAppearance->BorderSize = 0;
+			this->button7->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->button7->Font = (gcnew System::Drawing::Font(L"Nirmala UI", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->button7->ForeColor = System::Drawing::Color::White;
+			this->button7->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"button7.Image")));
+			this->button7->ImageAlign = System::Drawing::ContentAlignment::MiddleLeft;
+			this->button7->Location = System::Drawing::Point(0, 426);
+			this->button7->Name = L"button7";
+			this->button7->Padding = System::Windows::Forms::Padding(25, 0, 0, 0);
+			this->button7->Size = System::Drawing::Size(200, 50);
+			this->button7->TabIndex = 8;
+			this->button7->Text = L"  Connexion";
+			this->button7->TextAlign = System::Drawing::ContentAlignment::MiddleLeft;
+			this->button7->TextImageRelation = System::Windows::Forms::TextImageRelation::ImageBeforeText;
+			this->button7->UseVisualStyleBackColor = true;
+			// 
+			// button6
+			// 
+			this->button6->Dock = System::Windows::Forms::DockStyle::Bottom;
+			this->button6->FlatAppearance->BorderSize = 0;
+			this->button6->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->button6->Font = (gcnew System::Drawing::Font(L"Nirmala UI", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->button6->ForeColor = System::Drawing::Color::White;
+			this->button6->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"button6.Image")));
+			this->button6->Location = System::Drawing::Point(0, 511);
+			this->button6->Name = L"button6";
+			this->button6->Padding = System::Windows::Forms::Padding(25, 0, 0, 0);
+			this->button6->Size = System::Drawing::Size(200, 50);
+			this->button6->TabIndex = 7;
+			this->button6->Text = L"  Quitter";
+			this->button6->TextImageRelation = System::Windows::Forms::TextImageRelation::ImageBeforeText;
+			this->button6->UseVisualStyleBackColor = true;
+			this->button6->Click += gcnew System::EventHandler(this, &MyForm::button6_Click);
+			// 
+			// button5
+			// 
+			this->button5->Dock = System::Windows::Forms::DockStyle::Top;
+			this->button5->FlatAppearance->BorderSize = 0;
+			this->button5->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->button5->Font = (gcnew System::Drawing::Font(L"Nirmala UI", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->button5->ForeColor = System::Drawing::Color::White;
+			this->button5->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"button5.Image")));
+			this->button5->ImageAlign = System::Drawing::ContentAlignment::MiddleLeft;
+			this->button5->Location = System::Drawing::Point(0, 346);
+			this->button5->Name = L"button5";
+			this->button5->Padding = System::Windows::Forms::Padding(25, 0, 0, 0);
+			this->button5->Size = System::Drawing::Size(200, 50);
+			this->button5->TabIndex = 6;
+			this->button5->Text = L"  Statistiques";
+			this->button5->TextAlign = System::Drawing::ContentAlignment::MiddleLeft;
+			this->button5->TextImageRelation = System::Windows::Forms::TextImageRelation::ImageBeforeText;
+			this->button5->UseVisualStyleBackColor = true;
+			// 
+			// button4
+			// 
+			this->button4->Dock = System::Windows::Forms::DockStyle::Top;
+			this->button4->FlatAppearance->BorderSize = 0;
+			this->button4->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->button4->Font = (gcnew System::Drawing::Font(L"Nirmala UI", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->button4->ForeColor = System::Drawing::Color::White;
+			this->button4->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"button4.Image")));
+			this->button4->ImageAlign = System::Drawing::ContentAlignment::MiddleLeft;
+			this->button4->Location = System::Drawing::Point(0, 296);
+			this->button4->Name = L"button4";
+			this->button4->Padding = System::Windows::Forms::Padding(25, 0, 0, 0);
+			this->button4->Size = System::Drawing::Size(200, 50);
+			this->button4->TabIndex = 5;
+			this->button4->Text = L"  Stock";
+			this->button4->TextAlign = System::Drawing::ContentAlignment::MiddleLeft;
+			this->button4->TextImageRelation = System::Windows::Forms::TextImageRelation::ImageBeforeText;
+			this->button4->UseVisualStyleBackColor = true;
+			// 
+			// button3
+			// 
+			this->button3->Dock = System::Windows::Forms::DockStyle::Top;
+			this->button3->FlatAppearance->BorderSize = 0;
+			this->button3->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->button3->Font = (gcnew System::Drawing::Font(L"Nirmala UI", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->button3->ForeColor = System::Drawing::Color::White;
+			this->button3->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"button3.Image")));
+			this->button3->Location = System::Drawing::Point(0, 246);
+			this->button3->Name = L"button3";
+			this->button3->Padding = System::Windows::Forms::Padding(10, 0, 0, 0);
+			this->button3->Size = System::Drawing::Size(200, 50);
+			this->button3->TabIndex = 4;
+			this->button3->Text = L"  Commandes";
+			this->button3->TextAlign = System::Drawing::ContentAlignment::MiddleLeft;
+			this->button3->TextImageRelation = System::Windows::Forms::TextImageRelation::ImageBeforeText;
+			this->button3->UseVisualStyleBackColor = true;
+			// 
+			// button2
+			// 
+			this->button2->Dock = System::Windows::Forms::DockStyle::Top;
+			this->button2->FlatAppearance->BorderSize = 0;
+			this->button2->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->button2->Font = (gcnew System::Drawing::Font(L"Nirmala UI", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->button2->ForeColor = System::Drawing::Color::White;
+			this->button2->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"button2.Image")));
+			this->button2->Location = System::Drawing::Point(0, 196);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(200, 50);
+			this->button2->TabIndex = 3;
+			this->button2->Text = L"  Clients";
+			this->button2->TextAlign = System::Drawing::ContentAlignment::MiddleLeft;
+			this->button2->TextImageRelation = System::Windows::Forms::TextImageRelation::ImageBeforeText;
+			this->button2->UseVisualStyleBackColor = true;
+			// 
+			// button1
+			// 
+			this->button1->Dock = System::Windows::Forms::DockStyle::Top;
+			this->button1->FlatAppearance->BorderSize = 0;
+			this->button1->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->button1->Font = (gcnew System::Drawing::Font(L"Nirmala UI", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->button1->ForeColor = System::Drawing::Color::White;
+			this->button1->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"button1.Image")));
+			this->button1->Location = System::Drawing::Point(0, 146);
+			this->button1->Name = L"button1";
+			this->button1->Padding = System::Windows::Forms::Padding(5, 0, 0, 0);
+			this->button1->Size = System::Drawing::Size(200, 50);
+			this->button1->TabIndex = 2;
+			this->button1->Text = L"  Personnel";
+			this->button1->TextAlign = System::Drawing::ContentAlignment::MiddleLeft;
+			this->button1->TextImageRelation = System::Windows::Forms::TextImageRelation::ImageBeforeText;
+			this->button1->UseVisualStyleBackColor = true;
+			// 
+			// panel2
+			// 
+			this->panel2->Dock = System::Windows::Forms::DockStyle::Top;
+			this->panel2->Location = System::Drawing::Point(0, 115);
+			this->panel2->Name = L"panel2";
+			this->panel2->Size = System::Drawing::Size(200, 31);
+			this->panel2->TabIndex = 1;
+			// 
+			// panelLogo
+			// 
+			this->panelLogo->Controls->Add(this->pictureBox1);
+			this->panelLogo->Dock = System::Windows::Forms::DockStyle::Top;
+			this->panelLogo->Location = System::Drawing::Point(0, 0);
+			this->panelLogo->Name = L"panelLogo";
+			this->panelLogo->Size = System::Drawing::Size(200, 115);
+			this->panelLogo->TabIndex = 0;
+			// 
+			// pictureBox1
+			// 
+			this->pictureBox1->Image = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"pictureBox1.Image")));
+			this->pictureBox1->Location = System::Drawing::Point(0, 0);
+			this->pictureBox1->Name = L"pictureBox1";
+			this->pictureBox1->Size = System::Drawing::Size(200, 115);
+			this->pictureBox1->SizeMode = System::Windows::Forms::PictureBoxSizeMode::Zoom;
+			this->pictureBox1->TabIndex = 0;
+			this->pictureBox1->TabStop = false;
 			// 
 			// MyForm
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(740, 614);
+			this->BackColor = System::Drawing::Color::White;
+			this->ClientSize = System::Drawing::Size(934, 561);
 			this->Controls->Add(this->panel1);
+			this->MinimumSize = System::Drawing::Size(950, 600);
 			this->Name = L"MyForm";
 			this->Text = L"MyForm";
+			this->panel1->ResumeLayout(false);
+			this->panelLogo->ResumeLayout(false);
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->pictureBox1))->EndInit();
 			this->ResumeLayout(false);
 
 		}
 #pragma endregion
-	};
+	private: System::Void button6_Click(System::Object^ sender, System::EventArgs^ e) {
+
+		if (MessageBox::Show("Etes vous sur de vouloir quitter ?", "Quitter", MessageBoxButtons::YesNo) == System::Windows::Forms::DialogResult::Yes)
+		{
+			Application::Exit();
+		}
+	}
+};
 }
